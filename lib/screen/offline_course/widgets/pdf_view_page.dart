7252d845b7d96ff1bb679898f919a8792bc180b1
ishworpanta10 <ishworpanta10@gmail.com>
2021-08-13 16:08:10 +0545

    pdf error handling and some customization in pdf view page

--- lib/screen/offline_course/widgets/pdf_view_page.dart
+++ lib/screen/offline_course/widgets/pdf_view_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:native_pdf_view/native_pdf_view.dart';
 
+import '../../../constants/constants.dart';
+
 class PdfViewPage extends StatefulWidget {
   const PdfViewPage({required this.headingName, required this.pdfSource});
   final String headingName;
@@ -16,6 +18,7 @@ class _PdfViewPageState extends State<PdfViewPage> {
   void initState() {
     _pdfController = PdfController(
       document: PdfDocument.openAsset(widget.pdfSource),
+      initialPage: _initialPage,
     );
     super.initState();
   }
@@ -26,13 +29,70 @@ class _PdfViewPageState extends State<PdfViewPage> {
     super.dispose();
   }
 
+  final int _initialPage = 0;
+  int _actualPageNumber = 0, _allPagesCount = 0;
+
+  bool _scrollHorizontal = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.headingName),
+        actions: [
+          Container(
+            padding: const EdgeInsets.only(right: kPadding * 2),
+            alignment: Alignment.center,
+            child: Text(
+              '$_actualPageNumber/$_allPagesCount',
+              style: const TextStyle(fontSize: 18),
+            ),
+          ),
+        ],
       ),
-      body: PdfView(controller: _pdfController),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _scrollHorizontal,
+            onChanged: (value) {
+              setState(() {
+                _scrollHorizontal = value;
+              });
+            },
+            title: const Text("Scroll Horizontal"),
+          ),
+          Expanded(
+            child: PdfView(
+              documentLoader: const Center(child: CircularProgressIndicator()),
+              pageLoader: const Center(child: CircularProgressIndicator()),
+              controller: _pdfController,
+              scrollDirection: _scrollHorizontal ? Axis.horizontal : Axis.vertical,
+              onDocumentLoaded: (document) {
+                setState(() {
+                  _allPagesCount = document.pagesCount;
+                });
+              },
+              onPageChanged: (page) {
+                setState(() {
+                  _actualPageNumber = page;
+                });
+              },
+              errorBuilder: (_) {
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(Icons.error_outline, size: 40),
+                      SizedBox(height: kPadding),
+                      Text('Error Loading Pdf'),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
